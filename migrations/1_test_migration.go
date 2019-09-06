a72fb2a5ef86822b8ab9cb88d2fc4e4e501685b6
miguel.ramirez93 <miguel.ramirez@taximo.co>
2019-09-05 19:34:16 -0500

    #85 Se agrega libreria de migraciones y migracion de ejemplo

---
+++ migrations/1_test_migration.go
@@ -0,0 +1,34 @@
+package migrations
+
+import (
+	"context"
+
+	"github.com/udistrital/plan_cuentas_mongo_crud/models"
+
+	migrate "github.com/miguelramirez93/mongo-migrate"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func init() {
+	migrate.Register(func(db *mongo.Database) error {
+		_, err := db.Collection(models.MovimientoParameterCollection).InsertOne(context.TODO(), models.MovimientoParameter{
+			Multiplicador:       1,
+			TipoMovimientoHijo:  "test",
+			TipoMovimientoPadre: "test",
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	}, func(db *mongo.Database) error {
+		_, err := db.Collection(models.MovimientoParameterCollection).DeleteOne(context.TODO(), models.MovimientoParameter{
+			Multiplicador:       1,
+			TipoMovimientoHijo:  "test",
+			TipoMovimientoPadre: "test",
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
