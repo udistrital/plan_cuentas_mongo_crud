01f30879339ea9fec9abceeea69b4e28bf966fd8
juancsr <jcsarmientor@correo.udistrital.edu.co>
2019-12-12 15:58:20 -0500

    Servicios GET y PUT de documento presupuestal
    issue #187

--- controllers/documento_presupuestal.go
+++ controllers/documento_presupuestal.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"strconv"
 	"strings"
-
+	"encoding/json"
 	"github.com/astaxie/beego"
 	commonhelper "github.com/udistrital/plan_cuentas_mongo_crud/helpers/commonHelper"
 	documentopresupuestalmanager "github.com/udistrital/plan_cuentas_mongo_crud/managers/documentoPresupuestalManager"
@@ -13,6 +13,7 @@ import (
 
 type DocumentoPresupuestalController struct {
 	beego.Controller
+	response map[string]interface{}
 }
 
 // GetAllQuery función para obtener todos los objetos con la opción de hacer queries en la BD
@@ -51,6 +52,60 @@ func (j *DocumentoPresupuestalController) GetAllQuery() {
 	j.ServeJSON()
 }
 
+
+// Get ...
+// Get obtiene un elemento por su id
+// @Title Get
+// @Description get documento presupuestal by id
+// @Param	id		path 	string	true		"El id de la DocumentoPresupuestal a consultar"
+// @Success 200 {object} models.DocumentoPresupuestal
+// @Failure 403 :objectId is empty
+// @router /documento/:vigencia/:areaFuncional/:id [get]
+func (j *DocumentoPresupuestalController) Get() {
+	objectId := j.GetString(":id")
+	vigencia := j.GetString(":vigencia")
+	areaFuncional := j.GetString(":areaFuncional")
+	
+	docPresupuestal, err := models.GetDocumentoPresupuestalById(objectId, vigencia, areaFuncional)
+	if err == nil {
+		j.response = DefaultResponse(200, nil, &docPresupuestal)
+	} else {
+		j.response = DefaultResponse(403, err, nil)
+	}
+
+	j.Data["json"] = j.response
+	j.ServeJSON()
+}
+
+
+// Put de HTTP
+// @Title Update
+// @Description update a documento presupuestal document
+// @Param	id			path 	string							true		"The id you want to update"
+// @Param	body		body 	models.DocumentoPresupuestal	true		"The body"
+// @Success 200 {object} models.DocumentoPresupuestal
+// @Failure 403 :id is empty
+// @Failure 403 :vigencia is empty
+// @Failure 403 :areaFuncional is empty
+// @router /:vigencia/:areaFuncional/:id [put]
+func (j *DocumentoPresupuestalController) Put() {
+	objectID := j.Ctx.Input.Param(":id")
+	vigencia := j.Ctx.Input.Param(":vigencia")
+	areaFuncional := j.Ctx.Input.Param(":areaFuncional")
+
+	var docPresupuestal models.DocumentoPresupuestal
+
+	json.Unmarshal(j.Ctx.Input.RequestBody, &docPresupuestal)
+
+	if err := models.UpdateDocumentoPresupuestal(&docPresupuestal, objectID, vigencia, areaFuncional); err == nil {
+		j.response = DefaultResponse(200, nil, "update success!")
+	} else {
+		j.response = DefaultResponse(403, err, nil)
+	}
+
+	j.Data["json"] = j.response
+	j.ServeJSON()
+}
 // GetAll función para obtener todos los objetos
 // @Title GetAll
 // @Description get all objects
